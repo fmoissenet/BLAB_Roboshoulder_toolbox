5fe8b1e060ddc344d1bc2671138b47898e423738
Florent Moissenet <florent.moissenet@protonmail.com>
2023-08-04 17:12:57 +0200

    Change stl file read method

--- ImportBoneMesh.m
+++ ImportBoneMesh.m
@@ -10,7 +10,7 @@
 % -------------------------------------------------------------------------
 % Description:   Import bone models from STL files
 % -------------------------------------------------------------------------
-% Dependencies : None
+% Dependencies : - stlRead: https://github.com/petercorke/toolbox-common-matlab/blob/master/stlRead.m
 % -------------------------------------------------------------------------
 % This work is licensed under the Creative Commons Attribution - 
 % NonCommercial 4.0 International License. To view a copy of this license, 
@@ -24,66 +24,66 @@ function Bone = ImportBoneMesh(Subject,Bone)
 % HUMERUS
 % -------------------------------------------------------------------------
 stlFile                      = [Subject.id,'_',Subject.side,'_Humerus.stl'];
-temp                         = stlread(stlFile);
-Bone(1).Static.Mesh.vertices = permute(temp.Points*1e-3,[2,1,3]); % m
-Bone(1).Static.Mesh.faces    = permute(temp.ConnectivityList,[2,1,3]);
+[v,f,n]                      = stlRead(stlFile);
+Bone(1).Static.Mesh.vertices = permute(v*1e-3,[2,1,3]); % m
+Bone(1).Static.Mesh.faces    = permute(f,[2,1,3]);
 % Clear workspace
 clear stlFile temp;
 % Light version (reduced number of vertices, for illustration purposes)
 stlFile                       = [Subject.id,'_',Subject.side,'_Humerus_Light.stl'];
-temp                          = stlread(stlFile);
-Bone(1).Static.Meshl.vertices = permute(temp.Points*1e-3,[2,1,3]); % m
-Bone(1).Static.Meshl.faces    = permute(temp.ConnectivityList,[2,1,3]);
+[v,f,n]                       = stlRead(stlFile);
+Bone(1).Static.Meshl.vertices = permute(v*1e-3,[2,1,3]); % m
+Bone(1).Static.Meshl.faces    = permute(f,[2,1,3]);
 % Clear workspace
-clear stlFile temp;
+clear stlFile v f n;
 
 % -------------------------------------------------------------------------
 % SCAPULA
 % -------------------------------------------------------------------------
 stlFile                      = [Subject.id,'_',Subject.side,'_Scapula.stl'];
-temp                         = stlread(stlFile);
-Bone(2).Static.Mesh.vertices = permute(temp.Points*1e-3,[2,1,3]); % m
-Bone(2).Static.Mesh.faces    = permute(temp.ConnectivityList,[2,1,3]);
+[v,f,n]                      = stlRead(stlFile);
+Bone(2).Static.Mesh.vertices = permute(v*1e-3,[2,1,3]); % m
+Bone(2).Static.Mesh.faces    = permute(f,[2,1,3]);
 % Clear workspace
 clear stlFile temp;
 % Light version (reduced number of vertices, for illustration purposes)
 stlFile                       = [Subject.id,'_',Subject.side,'_Scapula_Light.stl'];
-temp                          = stlread(stlFile);
-Bone(2).Static.Meshl.vertices = permute(temp.Points*1e-3,[2,1,3]); % m
-Bone(2).Static.Meshl.faces    = permute(temp.ConnectivityList,[2,1,3]);
+[v,f,n]                       = stlRead(stlFile);
+Bone(2).Static.Meshl.vertices = permute(v*1e-3,[2,1,3]); % m
+Bone(2).Static.Meshl.faces    = permute(f,[2,1,3]);
 % Clear workspace
-clear stlFile temp;
+clear stlFile v f n;
 
 % -------------------------------------------------------------------------
 % CLAVICLE
 % -------------------------------------------------------------------------
 stlFile                      = [Subject.id,'_',Subject.side,'_Clavicle.stl'];
-temp                         = stlread(stlFile);
-Bone(3).Static.Mesh.vertices = permute(temp.Points*1e-3,[2,1,3]); % m
-Bone(3).Static.Mesh.faces    = permute(temp.ConnectivityList,[2,1,3]);
+[v,f,n]                      = stlRead(stlFile);
+Bone(3).Static.Mesh.vertices = permute(v*1e-3,[2,1,3]); % m
+Bone(3).Static.Mesh.faces    = permute(f,[2,1,3]);
 % Clear workspace
 clear stlFile temp;
 % Light version (reduced number of vertices, for illustration purposes)
 stlFile                       = [Subject.id,'_',Subject.side,'_Clavicle_Light.stl'];
-temp                          = stlread(stlFile);
-Bone(3).Static.Meshl.vertices = permute(temp.Points*1e-3,[2,1,3]); % m
-Bone(3).Static.Meshl.faces    = permute(temp.ConnectivityList,[2,1,3]);
+[v,f,n]                       = stlRead(stlFile);
+Bone(3).Static.Meshl.vertices = permute(v*1e-3,[2,1,3]); % m
+Bone(3).Static.Meshl.faces    = permute(f,[2,1,3]);
 % Clear workspace
-clear stlFile temp;
+clear stlFile v f n;
 
 % -------------------------------------------------------------------------
 % THORAX
 % -------------------------------------------------------------------------
 stlFile                      = [Subject.id,'_',Subject.side,'_Thorax.stl'];
-temp                         = stlread(stlFile);
-Bone(4).Static.Mesh.vertices = permute(temp.Points*1e-3,[2,1,3]); % m
-Bone(4).Static.Mesh.faces    = permute(temp.ConnectivityList,[2,1,3]);
+[v,f,n]                      = stlRead(stlFile);
+Bone(4).Static.Mesh.vertices = permute(v*1e-3,[2,1,3]); % m
+Bone(4).Static.Mesh.faces    = permute(f,[2,1,3]);
 % Clear workspace
 clear stlFile temp;
 % Light version (reduced number of vertices, for illustration purposes)
 stlFile                       = [Subject.id,'_',Subject.side,'_Thorax_Light.stl'];
-temp                          = stlread(stlFile);
-Bone(4).Static.Meshl.vertices = permute(temp.Points*1e-3,[2,1,3]); % m
-Bone(4).Static.Meshl.faces    = permute(temp.ConnectivityList,[2,1,3]);
+[v,f,n]                       = stlRead(stlFile);
+Bone(4).Static.Meshl.vertices = permute(v*1e-3,[2,1,3]); % m
+Bone(4).Static.Meshl.faces    = permute(f,[2,1,3]);
 % Clear workspace
-clear stlFile temp;
+clear stlFile v f n;
